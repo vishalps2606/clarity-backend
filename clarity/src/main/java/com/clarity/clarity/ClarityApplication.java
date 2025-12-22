e430a0c6ffc9cb712f91e56aff16f556e6312e43
vishalps2606 <vishalps2606@gmail.com>
2025-12-23 00:51:28 +0530

    feat: Initial spring boot project setup

---
+++ clarity/src/main/java/com/clarity/clarity/ClarityApplication.java
@@ -0,0 +1,13 @@
+package com.clarity.clarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarityApplication.class, args);
+	}
+
+}
---
+++ clarity/src/test/java/com/clarity/clarity/ClarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarity.clarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
