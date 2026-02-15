539482f6ae72beb697ac9841388f1f2a28b60617
vishalps2606 <vishalps2606@gmail.com>
2026-02-15 13:10:29 +0530

    Day 28: Fixes the mobile setup issues

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('nativewind/preset')],
   // NOTE: This points to App.tsx and the src folder
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
